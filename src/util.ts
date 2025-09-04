ab89c100a38d4544e16babb29cacc20f3523cca7
mikaello <2505178+mikaello@users.noreply.github.com>
2025-09-04 13:38:51 +0000

    docs: improve branch postfix description

--- src/util.ts
+++ src/util.ts
@@ -28,9 +28,23 @@ export function makeResource(
 /**
  * Useful utility when you are creating review environments in GitLab.
  * This creates a branch postfix you can use to differientiate resources when creating review environments.
- * When on default branch it will be empty string ("").
+ *
+ * When on default branch it will be empty string (""). When on feature branch it will be a dash ("-")
+ * followed by a substring of the hash of the branch name.
  *
  * **NB**: GitLab users only!
+ *
+ * @example Add branch prefix for rule group
+ * ```ts
+ * import { BRANCH_POSTFIX } from '@mikaello/grizzly-foundation-sdk-ts';
+ *
+ *  new RuleGroupBuilder('GitLab')
+ *    .withRule(
+ *      new RuleBuilder('Prod Uptime')
+ *        .uid(`gitlab-prod-uptime${BRANCH_POSTFIX}`)
+ *        // ...
+ *     )
+ * ```
  */
 export const BRANCH_POSTFIX =
   process.env.CI_COMMIT_REF_SLUG !== undefined &&
